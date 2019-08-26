188dc08e8dac43190f915e0c3e38d6a61867454a
TLSvdAvoort <somegamemusic@gmail.com>
2019-08-26 14:30:33 +0200

    groep en leerling overzicht

--- public/groep.php
+++ public/groep.php
@@ -16,25 +16,27 @@
     <?php
         require '../app/connectie.php';
 
-        $sqlleerlingen = "
-        SELECT leerlingen.voornaam, leerlingen.tussenvoegsel, leerlingen.achternaam, leerlingen.Groep_id, leerlingen.Level, cohort.Cohort
+        $sqlgroepen = "
+        SELECT leerlingen.leerling_ID, leerlingen.voornaam, leerlingen.tussenvoegsel, leerlingen.achternaam, leerlingen.Groep_id, leerlingen.Level, cohort.Cohort
         FROM `leerlingen`
-        INNER JOIN `cohort` ON leerlingen.Cohort=cohort.Cohort_ID";
+        INNER JOIN `cohort` ON leerlingen.Cohort=cohort.Cohort_ID
+        WHERE leerlingen.Groep_id = $_GET[id]";
 
         $table[] = "
             <table class='striped'>
                 <thead>
                     <tr>
-                        <th class='col m6 s12 offset-m3'>Voornaam</th>
-                        <th class='col m6 s12 offset-m3'>Tussenvoegsel</th>
-                        <th class='col m6 s12 offset-m3'>Achternaam</th>
-                        <th class='col m6 s12 offset-m3'>Groep</th>
-                        <th class='col m6 s12 offset-m3'>Level</th>
-                        <th class='col m6 s12 offset-m3'>Cohort</th>
+                        <th class=' '>Voornaam</th>
+                        <th class=' '>Tussenvoegsel</th>
+                        <th class=' '>Achternaam</th>
+                        <th class=' '>Groep</th>
+                        <th class=' '>Level</th>
+                        <th class=' '>Cohort</th>
+                        <th class=' '>LOG</th>
                     </tr>
                 </thead>";
 
-        $result = mysqli_query($conn, $sqlleerlingen) or die(mysqli_error($conn));
+        $result = mysqli_query($conn, $sqlgroepen) or die(mysqli_error($conn));
 
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
@@ -45,6 +47,7 @@
                            <td>" . $row["Groep_id"] . "</td>
                            <td>" . $row["Level"] . "</td>
                            <td>" . $row["Cohort"] . "</td>
+                           <td><a href='leerling.php?id=". $row['leerling_ID']. "'>log</a></td>
                        </tr>";
             }
             echo "</table>";
@@ -52,14 +55,15 @@
             echo "Error";
         }
 
-        echo "<div class='row'>";
+        echo "<div class='row'><div class='col s6'>";
         // Table aanmaken
         if (isset($table)) {
             foreach ($table as $key => $table_done) {
                 echo $table_done;
             }
         };
-        echo "</div>";
+        echo "</div></div>";
     ?>
+    <button><a href="kaart.php">Kaart</a></button>
 </body>
 </html>
