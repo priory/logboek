bf877792bb2faf1b1a46b8b77abf25bb9b019290
priory <v.packo94@gmail.com>
2019-08-26 11:39:58 +0200

    Add app folder, add login form

--- public/index.php
+++ public/index.php
@@ -1,10 +1,30 @@
 <html>
-    <?php 
-        $root = __DIR__ . '\..\\';
+    <head>
+        <?php 
+            $root = __DIR__ . '\..\\';
 
-        require_once $root . 'resources\\layouts\\head.php';
-    ?>
+            require_once $root . 'resources\\layouts\\head.php';
+        ?>
+    </head>
     <body>
-
+        <div class="row">
+            <form class="col m6 s12 offset-m3" name="form" method="POST" action="./authenticate.php" onsubmit="return false;">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="user" type="text">
+                        <label for="user">Gebruiker</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="password" type="password">
+                        <label for="password">Wachtwoord</label>
+                    </div>
+                <div class="row">
+                    <div class="btn col m6 s12 offset-m3" onclick="document.form.submit();">Login</div>
+                </div>
+            </form>
+        </div>
+  </div>
     </body>
 </html>
--- resources/layouts/head.php
+++ resources/layouts/head.php
@@ -1,10 +1,15 @@
-<head>
-    <script
+<script
         src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</head>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script>
+    $(function () {
+        
+    });
+</script>
+
