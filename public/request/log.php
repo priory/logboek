06c13cb3e48c0b6fc3cdd6edbbc434a6268c83ee
priory <v.packo94@gmail.com>
2019-08-26 14:19:32 +0200

    Add log request

---
+++ public/request/log.php
@@ -0,0 +1,15 @@
+<?php
+switch ($_POST['method']) {
+    case 'GET':
+        
+        break;
+    case 'STORE':
+
+        break;
+    case 'UPDATE':
+
+        break;
+    case 'DELETE':
+
+        break;
+}
