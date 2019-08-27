5b9ce25c9d5de7247dca613c98d9886fc7e07ea8
priory <v.packo94@gmail.com>
2019-08-27 08:46:36 +0200

    Add mocking to log

--- public/request/log.php
+++ public/request/log.php
@@ -1,23 +1,34 @@
 <?php
+
 switch ($_POST['method']) {
     case 'GET':
         $group = $_POST['groep'];
         $leerling = $_POST['leerling'];
         $id = $_POST['id'];
 
+        $logs = ['id' => 1, 'content' => 'mock', 'date' => '2019-08-27 12:00:00'];
+
+        echo json_encode($logs);
+
         break;
     case 'STORE':
         $id = $_POST['id'];
         $content = $_POST['content'];
+
+        echo true;
 
         break;
     case 'UPDATE':
         $id = $_POST['id'];
         $content = $_POST['content'];
 
+        echo true;
+
         break;
     case 'DELETE':
         $id = $_POST['id'];
-        
+
+        echo true;
+
         break;
 }
