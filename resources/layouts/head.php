018154bb83a305ae6f03a16b1bfa81ab601dfeb6
priory <v.packo94@gmail.com>
2019-08-29 12:48:42 +0200

    Add mobile navbar, Add fixed navbar

--- resources/layouts/head.php
+++ resources/layouts/head.php
@@ -1,7 +1,7 @@
 <script
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous"></script>
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -28,7 +28,7 @@
     }
 
     $(function () {
-        
+        $('.sidenav').sidenav();
     });
 </script>
 
--- resources/layouts/nav.php
+++ resources/layouts/nav.php
@@ -1,9 +1,20 @@
-<nav>
-    <div class="nav-wrapper indigo lighten-2">
-      <a href="logout.php" class="brand-logo right">Logout</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="kaart.php">Kaart</a></li>
-        <li><a href="javascript:history.back()">Terug</a></li>
-      </ul>
-    </div>
-  </nav>
+<div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper indigo lighten-2">
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="hide-on-med-and-down">
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="logout.php">Log uit</a></li>
+                    <li><a href="kaart.php">Kaart</a></li>
+                    <li><a href="javascript:history.back()">Terug</a></li>
+                </ul>
+            </ul>
+        </div>
+    </nav>
+</div>
+
+<ul class="sidenav" id="mobile-demo">
+    <li><a href="kaart.php">Kaart</a></li>
+    <li><a href="javascript:history.back()">Terug</a></li>
+    <li><a href="logout.php">Log uit</a></li>
+</ul>
